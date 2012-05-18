e32db8606ebbafb0d80dc0ac19ab94901a7fb7a0
Aaron Parecki <aaron@parecki.com>
2012-05-18 12:16:03 -0700

    Updates SDK to 12.139. Adds SDK name to tracking headers.

--- IOS/GeoloqiSDK/LQSDKVersion.h
+++ IOS/GeoloqiSDK/LQSDKVersion.h
@@ -5,8 +5,13 @@
 //  Copyright (c) 2012 Geoloqi, Inc. All rights reserved.
 //
 
+#ifdef LQ_TITANIUM
+static NSString *const LQSDKNameString = @"com.geoloqi.titanium.iphone";
+#else
+static NSString *const LQSDKNameString = @"com.geoloqi.iphone";
+#endif
 
-static NSString *const LQSDKVersionString = @"12.134";
+static NSString *const LQSDKVersionString = @"12.139";
 
 // This build string is automatically updated when the sdk is built! Do not modify.
-static NSString *const LQSDKVersionGitVersion = @"df5ae7c4745f0bdf64404733bfc5d1678f701552";
+static NSString *const LQSDKVersionGitVersion = @"e73022cfda5abfc8b766e32856cd37711eb1c7fb";
